0a667a5cf5d31c05aa9d187e127ee778e615c1c9
tiencahi chamnan <kanompang962@gmail.com>
2022-10-12 23:26:13 +0700

    CRUD

--- projectNET-ASP/Controllers/CallStudentController.cs
+++ projectNET-ASP/Controllers/CallStudentController.cs
@@ -2,24 +2,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using projectNET_ASP.Models;
+using System.Text;
 
 namespace projectNET_ASP.Controllers
 {
     public class CallStudentController : Controller
     {
         HttpClientHandler _clientHandler = new HttpClientHandler();
+        // GET: CallIssueController
         public CallStudentController()
         {
             _clientHandler.ServerCertificateCustomValidationCallback =
                 (sender, cert, chain, sslPolicyErrors) => { return true; };
         }
-        // GET: CallStudentController
         public async Task<ActionResult> Index()
         {
             var student = await GetStudent();
             return View(student);
         }
-
         [HttpGet]
         public async Task<List<Student>> GetStudent()
         {
@@ -34,62 +34,108 @@ namespace projectNET_ASP.Controllers
             }
             return studentList;
         }
-
-        // GET: CallStudentController/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
         {
-            return View();
+            Student student = new Student();
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7291/api/Student/id?id=" + id))
+                {
+                    string strJson = await response.Content.ReadAsStringAsync();
+                    student = JsonConvert.DeserializeObject<Student>(strJson);
+                }
+            }
+            return View(student);
         }
 
-        // GET: CallStudentController/Create
+
+
+        // GET: CallIssueController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: CallStudentController/Create
+        // POST: CallIssueController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(Student student)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                Student std = new Student();
+                using (var httpClient = new HttpClient(_clientHandler))
+                {
+                    StringContent content = new StringContent(JsonConvert.SerializeObject(student), Encoding.UTF8, "application/json");
+                    using (var response = await httpClient.PostAsync("https://localhost:7291/api/Student", content))
+                    {
+                        string strJson = await response.Content.ReadAsStringAsync();
+                        std = JsonConvert.DeserializeObject<Student>(strJson);
+                        if (ModelState.IsValid)
+                        {
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                }
+                return View(std);
             }
             catch
             {
                 return View();
             }
+
         }
 
-        // GET: CallStudentController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: CallIssueController/Edit/5
+        public async Task<ActionResult> Edit(int id)
         {
-            return View();
+            Student student = new Student();
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7291/api/Student/id?id=" + id))
+                {
+                    string strJson = await response.Content.ReadAsStringAsync();
+                    student = JsonConvert.DeserializeObject<Student>(strJson);
+                }
+            }
+            return View(student);
         }
 
-        // POST: CallStudentController/Edit/5
+        // POST: CallIssueController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(int id, Student student)
         {
-            try
+            string del = "";
+            using (var httpClient = new HttpClient(_clientHandler))
             {
-                return RedirectToAction(nameof(Index));
+                StringContent content =
+                    new StringContent(JsonConvert.SerializeObject(student), Encoding.UTF8, "application/json");
+                using (var response = await httpClient.PutAsync("https://localhost:7291/api/Student/" + id, content))
+                {
+                    del = await response.Content.ReadAsStringAsync();
+                }
             }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction(nameof(Index));
+
+
         }
 
-        // GET: CallStudentController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: CallIssueController/Delete/5
+        public async Task<ActionResult> Delete(int id)
         {
-            return View();
+            string del = "";
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.DeleteAsync("https://localhost:7291/api/Student/" + id))
+                {
+                    del = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return RedirectToAction(nameof(Index));
         }
 
-        // POST: CallStudentController/Delete/5
+        // POST: CallIssueController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
